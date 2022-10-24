68bdf87a846cc048a0d956480de8586c71e56f3b
G00402296 <G00402296@atu.ie>
2022-10-24 09:43:09 +0100

    Added Enter Student Name Prompt

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,6 +1,12 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
+        System.out.println("Please Enter A Student Name");
+        Scanner input = new Scanner(System.in);
+        String studentName = input.next();
+
     }
 }
