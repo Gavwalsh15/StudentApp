4c50d7453c4b5ae05f8f94eb39d566a30c84b02d
G00402296 <G00402296@atu.ie>
2022-10-24 10:15:55 +0100

    Added new file called Student

---
+++ src/main/java/ie/atu/Student.java
@@ -0,0 +1,31 @@
+package ie.atu;
+
+public class Student {
+    private String studName;
+    private String studEmail;
+    private String studCourse;
+
+    public String getStudName() {
+        return this.studName;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public String getStudEmail() {
+        return this.studEmail;
+    }
+
+    public void setStudEmail(String studEmail) {
+        this.studEmail = studEmail;
+    }
+
+    public String getStudCourse() {
+        return this.studCourse;
+    }
+
+    public void setStudCourse(String studCourse) {
+        this.studCourse = studCourse;
+    }
+}
